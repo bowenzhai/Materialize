2d1789c07d3d57332613490c48e464f7d6d0a25e
Bowen Zhai <kevinzhai97@gmail.com>
2016-04-24 00:25:23 -0400

    Splash screen theme changed

--- app/src/main/java/com/bowenzhai/materialize/MainActivity.java
+++ app/src/main/java/com/bowenzhai/materialize/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bowenzhai.materialize;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +10,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.splash_page);
     }
 
     public void onSplashPageClick(View view){
-        finish();
+        Intent intent = new Intent(this, Questions.class);
+        startActivity(intent);
     }
 }
