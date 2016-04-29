001c96ad69db5194be49f1fb74e726b35ff89eff
Bowen Zhai <kevinzhai97@gmail.com>
2016-04-29 16:40:47 -0400

    Fancy animations

--- app/src/main/java/com/bowenzhai/materialize/Questions.java
+++ app/src/main/java/com/bowenzhai/materialize/Questions.java
@@ -3,7 +3,11 @@ package com.bowenzhai.materialize;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Questions extends AppCompatActivity {
@@ -12,6 +16,9 @@ public class Questions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+        findViewById(R.id.tickcross).setVisibility(View.INVISIBLE);
+        findViewById(R.id.correctornot).setVisibility(View.INVISIBLE);
+        findViewById(R.id.nextbutton).setVisibility(View.INVISIBLE);
     }
 
     public void onHintClick(View view) {
@@ -23,13 +30,42 @@ public class Questions extends AppCompatActivity {
         String answer = ((EditText)findViewById(R.id.answer)).getText().toString();
         answer = answer.toLowerCase();
         String correctAnswer = getString(R.string.A1);
+        correctAnswer = correctAnswer.toUpperCase();
+        answer = answer.toUpperCase();
 
         if (answer.equals(correctAnswer)) {
-            Toast toasty = Toast.makeText(getApplicationContext(), "Right!", Toast.LENGTH_SHORT);
-            toasty.show();
+            TextView t = (TextView) findViewById(R.id.correctornot);
+            t.setText("CORRECT!");
+            ImageView i = (ImageView) findViewById(R.id.tickcross);
+            i.setImageDrawable(getDrawable(R.drawable.tick));
+            answerSubmitted();
         } else {
-            Toast toasty = Toast.makeText(getApplicationContext(), "Nope!", Toast.LENGTH_SHORT);
-            toasty.show();
+            TextView t = (TextView) findViewById(R.id.correctornot);
+            t.setText("CORRECT ANSWER: " + correctAnswer);
+            ImageView i = (ImageView) findViewById(R.id.tickcross);
+            i.setImageDrawable(getDrawable(R.drawable.cross));
+            answerSubmitted();
         }
+    }
+
+    public void answerSubmitted() {
+        findViewById(R.id.tickcross).setVisibility(View.VISIBLE);
+        TranslateAnimation animation = new TranslateAnimation(0,0,2000,0);
+        animation.setDuration(1000);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                findViewById(R.id.correctornot).setVisibility(View.VISIBLE);
+                findViewById(R.id.nextbutton).setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+        findViewById(R.id.tickcross).startAnimation(animation);
+
     }
 }
